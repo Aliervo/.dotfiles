9f7743de655fc149e2ca9a40a737e2c6d8688b26
Aliervo <aliervo@wingedvengeance.net>
2022-05-17 19:18:47 -0500

    Add snippet from Packer.nvim wiki to allow bootstrapping

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,33 +1,44 @@
--- Packer plugin setup
-require('packer').startup(function()
+-- Bootstrap Packer if it isn't already installed
+local fn = vim.fn
+local install_path = fn.stdpath("data").."/site/pack/packer/start/packer.nvim"
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
+
+require('packer').startup(function(use)
 	-- Have packer manage itself
 	use 'wbthomason/packer.nvim'
 
 	-- Enable treesitter with post-install/update hook
 	use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
 
-	-- Enable the lspconfig
+	-- Enable the lspconfig with auto server installation
 	use { 
-    "williamboman/nvim-lsp-installer",
-    {
-      'neovim/nvim-lspconfig',
-      config = function()
-        require("nvim-lsp-installer").setup{
-          automatic_installation = true
-        }
-        local lspconfig = require("lspconfig")
-        lspconfig.tsserver.setup{}
-        lspconfig.html.setup{}
-        lspconfig.svelte.setup{}
-      end
-    }
-  }
+	    "williamboman/nvim-lsp-installer",
+	    {
+	      'neovim/nvim-lspconfig',
+	      config = function()
+		require("nvim-lsp-installer").setup{
+		  automatic_installation = true
+		}
+		local lspconfig = require("lspconfig")
+		lspconfig.tsserver.setup{}
+		lspconfig.html.setup{}
+		lspconfig.svelte.setup{}
+	      end
+	    }
+	  }
 
   -- Enable LuaSnip
   use 'L3MON4D3/LuaSnip'
 
   -- Enable Ledger Support
   use 'ledger/vim-ledger'
+
+  -- Automatically set up configuration after cloning packer.nvim
+  if packer_bootstrap then
+    require("packer").sync()
+  end
 end)
 
 -- Automatically run :PackerSync whenever plugins.lua is updated
