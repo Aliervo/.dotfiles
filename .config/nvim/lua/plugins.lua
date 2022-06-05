a1463d33d037e84e7de2d1b6e77cf4a1d9de0eea
Aliervo <aliervo@wingedvengeance.net>
2022-06-05 06:31:39 -0500

    Add --no-startup-id to pipewire autostart

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -21,16 +21,31 @@ require('packer').startup(function(use)
           require("nvim-lsp-installer").setup{
             automatic_installation = true
           }
+          local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
           local lspconfig = require("lspconfig")
-          lspconfig.tsserver.setup{}
-          lspconfig.html.setup{}
-          lspconfig.svelte.setup{}
+          lspconfig.tsserver.setup{
+            capabilities = capabilities
+          }
+          lspconfig.html.setup{
+            capabilities = capabilities
+          }
+          lspconfig.svelte.setup{
+            capabilities = capabilities
+          }
               end
             }
           }
 
-  -- Enable LuaSnip
-  use 'L3MON4D3/LuaSnip'
+  -- Enable Autocompletion and Snippets
+  use {
+    "L3MON4D3/LuaSnip",
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-cmdline",
+    "hrsh7th/nvim-cmp",
+    "saadparwaiz1/cmp_luasnip"
+  }
 
   -- Enable Ledger Support
   use 'ledger/vim-ledger'
@@ -47,6 +62,45 @@ end)
 -- Configure Autopairs
 require("nvim-autopairs").setup({ map_cr = true })
 
+-- Configure Autocomplete and Snippets
+local cmp = require("cmp")
+
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      require("luasnip").lsp_expand(arg.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.abort(),
+    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+  }),
+  sources = cmp.config.sources({
+    { name = "nvim_lsp" },
+    { name = "luasnip" },
+  }, {
+    { name = "buffer" },
+  })
+})
+
+cmp.setup.cmdline("/", {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = "buffer" }
+  }
+})
+
+cmp.setup.cmdline(":", {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = "path" }
+  }, {
+    { name = "cmdline" }
+  })
+})
 -- Automatically run :PackerSync whenever plugins.lua is updated
 vim.cmd([[autocmd BufWritePost plugins.lua source <afile> | PackerSync]])
 
