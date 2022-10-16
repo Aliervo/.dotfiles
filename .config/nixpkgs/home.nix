f50d2a8fc4598cf8b623eb5a1fcd7c73bb8a37a6
Aliervo <aliervo@wingedvengeance.net>
2022-10-16 16:16:13 -0500

    setup standalone pnpm

--- .config/nixpkgs/home.nix
+++ .config/nixpkgs/home.nix
@@ -12,6 +12,8 @@ with pkgs;{
   home.packages = with pkgs; [
     gomuks
     authy
+    android-tools
+    gh
   ];
 
   # This value determines the Home Manager release that your
@@ -27,51 +29,72 @@ with pkgs;{
   # Allow unfree pkgs to be installed.
   nixpkgs.config.allowUnfree = true;
 
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
+  programs = {
+    alacritty.enable = true;
+    alacritty.settings = {
+      env.TERM = "xterm-256color";
+      window.dynamic_title = true;
+      font = {
+        normal = {
+          family = "Victor Mono";
+          style = "Regular";
+        };
+        bold.style = "Bold";
+        italic.style = "Italic";
+        bold_italic.style = "Bold Italic";
+        size = 8.0;
+      };
+    };
 
-  # Git configuration
-  programs.git = {
-    enable = true;
-    aliases = { co = "checkout"; };
-    userEmail = "aliervo@wingedvengeance.net";
-    userName = "Aliervo";
-  };
+    git = {
+      enable = true;
+      aliases = { co = "checkout"; };
+      userEmail = "aliervo@wingedvengeance.net";
+      userName = "Aliervo";
+    };
 
-  # Zsh configuration
-  programs.zsh = {
-    enable = true;
-    autocd = true;
-    dotDir = ".config/zsh";
-    envExtra = ''
-      export TERMINAL="alacritty"
-      export QT_QPA_PLATFORMTHEME=gtk2
-      export EDITOR=$(which nvim)
-      export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME/npm/npmrc"
-      export _JAVA_OPTIONS=-Djava.util.prefs.userRoot="$XDG_CONFIG_HOME"/java
-      export GTK2_RC_FILES="$XDG_CONFIG_HOME"/gtk-2.0/gtkrc
-      export GRADLE_USER_HOME="$XDG_DATA_HOME"/gradle
-      export NIX_PATH=$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels''${NIX_PATH:+:$NIX_PATH}
-    '';
-    history.path = "${config.xdg.dataHome}/zsh/histfile";
-    initExtra = ''
-      # Initialise the prompt system and set prompt to Pure.
-      autoload -U promptinit; promptinit
-      prompt pure
-    '';
-    plugins = [
-      {
-        name = "pure";
-        src = pkgs.fetchFromGitHub {
-            owner = "sindresorhus";
-            repo = "pure";
-            rev = "v1.20.1";
-            sha256 = "iuLi0o++e0PqK81AKWfIbCV0CTIxq2Oki6U2oEYsr68=";
-          };
-      }
-    ];
-    shellAliases = {
-      dotfiles = "$(which git) --git-dir=$HOME/.dotfiles/ --work-tree=$HOME";
+    home-manager.enable = true;
+
+    zsh = {
+      enable = true;
+      autocd = true;
+      dotDir = ".config/zsh";
+      envExtra = ''
+        export TERMINAL="alacritty"
+        export QT_QPA_PLATFORMTHEME=gtk2
+        export EDITOR=$(which nvim)
+        export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME/npm/npmrc"
+        export _JAVA_OPTIONS=-Djava.util.prefs.userRoot="$XDG_CONFIG_HOME"/java
+        export GTK2_RC_FILES="$XDG_CONFIG_HOME"/gtk-2.0/gtkrc
+        export GRADLE_USER_HOME="$XDG_DATA_HOME"/gradle
+        export NIX_PATH=$HOME/.nix-defexpr/channels''${NIX_PATH:+:$NIX_PATH}
+        export PNPM_HOME=${config.xdg.dataHome}/pnpm
+      '';
+      history.path = "${config.xdg.dataHome}/zsh/histfile";
+      initExtra = ''
+        # Initialise the prompt system and set prompt to Pure.
+        autoload -U promptinit; promptinit
+        prompt pure
+
+        # Adjust $PATH
+        typeset -U path PATH
+        path=($PNPM_HOME $HOME/.local/bin $path)
+        export PATH
+      '';
+      plugins = [
+        {
+          name = "pure";
+          src = pkgs.fetchFromGitHub {
+              owner = "sindresorhus";
+              repo = "pure";
+              rev = "v1.20.1";
+              sha256 = "iuLi0o++e0PqK81AKWfIbCV0CTIxq2Oki6U2oEYsr68=";
+            };
+        }
+      ];
+      shellAliases = {
+        dotfiles = "$(which git) --git-dir=$HOME/.dotfiles/ --work-tree=$HOME";
+      };
     };
   };
 }
