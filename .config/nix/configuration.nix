ef1b408a0d2b376d5f3286c7224b40eaee9ab709
Aliervo <samfritz@protonmail.com>
2024-02-22 17:01:18 -0600

    Enable easy use of AppImages and add some comments

--- .config/nix/configuration.nix
+++ .config/nix/configuration.nix
@@ -27,6 +27,17 @@
   };
 
   boot = {
+    # Tell the kernel to use appimage-run as the interpreter for appimage binary
+    # files with the magic number "AI" + 0x02
+    binfmt.registrations.appimage = {
+      wrapInterpreterInShell = false;
+      interpreter = "${pkgs.appimage-run}/bin/appimage-run";
+      recognitionType = "magic";
+      offset = 0;
+      mask = ''\xff\xff\xff\xff\x00\x00\x00\x00\xff\xff\xff'';
+      magicOrExtension = ''\x7fELF....AI\x02'';
+    };
+
     loader = {
       systemd-boot.enable = true;
       efi = {
@@ -60,9 +71,9 @@
       # Hybrid Graphics Settings
       prime = {
         offload = {
-	  enable = true;
-	  enableOffloadCmd = true;
-	};
+          enable = true;
+          enableOffloadCmd = true;
+        };
 
         intelBusId = "PCI:0:2:0";
         nvidiaBusId = "PCI:1:0:0";
@@ -79,6 +90,8 @@
 
     hostName = "nixClam";
 
+    # Only enable this OR NetworkManager, not both
+    # Use NetworkManager to play with WiDi
     wireless.iwd = {
       enable = true;
       settings = {
@@ -86,6 +99,8 @@
         Settings.AutoConnect = true;
       };
     };
+    # If using NetworkManager, add users to the networkmanager group
+    networkmanager.enable = false;
   };
 
   nix = {
@@ -129,7 +144,7 @@
   users.users.aliervo = {
     isNormalUser = true;
     description = "Sam";
-    extraGroups = [ "wheel" "video" ];
+    extraGroups = [ "wheel" /* "networkmanager" */ "video" ];
     shell = pkgs.zsh;
   };
 
@@ -143,6 +158,7 @@
 
     systemPackages = with pkgs; [
       htop
+      # gnome-network-displays
     ];
   };
 
@@ -184,7 +200,6 @@
 
   # List services that you want to enable:
   services = {
-
     # Enable session agnostic keybindings
     actkbd = {
       enable = true;
